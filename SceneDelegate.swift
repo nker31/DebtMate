7e3f8b7bf7188c4d9856c5dd45084065ec58259b
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-12-05 14:42:24 +0700

    refactor: update SceneDelegate entry point

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -10,12 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    let authenticationManager: AuthenticationManagerProtocol = AuthenticationManager.shared
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: LoginViewController())
+        
+        if let _ = authenticationManager.userSession {
+            window.rootViewController = UINavigationController(rootViewController: MainViewController())
+        } else {
+            window.rootViewController = UINavigationController(rootViewController: LoginViewController())
+        }
+        
         self.window = window
         self.window?.makeKeyAndVisible()
     }
