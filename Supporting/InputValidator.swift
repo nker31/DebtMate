edc98e0a0a080150a161f7e19e15ac5d139b2c79
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-12-05 13:04:59 +0700

    feat: add validateFullName to InputValidator

--- Supporting/InputValidator.swift
+++ Supporting/InputValidator.swift
@@ -9,12 +9,19 @@ import Foundation
 
 class InputValidator {
     static func validateEmail(_ email: String) -> Bool {
+        let email = email.trimmingCharacters(in: .whitespacesAndNewlines)
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return emailPredicate.evaluate(with: email)
     }
     
     static func validatePassword(_ password: String) -> Bool {
+        let password = password.trimmingCharacters(in: .whitespacesAndNewlines)
         return password.count >= 8 && password.count <= 12
+    }
+    
+    static func validateFullName(_ fullName: String) -> Bool {
+        let fullName = fullName.trimmingCharacters(in: .whitespacesAndNewlines)
+        return fullName.count >= 3 && fullName.count <= 25
     }
 }
