32dd84dac16d4882af219835c4a882285cbe22d4
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-12-09 16:43:55 +0700

    feat: Add changePassword func to AuthenticationManager

--- Manager/AuthenticationManager.swift
+++ Manager/AuthenticationManager.swift
@@ -13,6 +13,7 @@ protocol AuthenticationManagerProtocol {
     var userSession: FirebaseAuth.User? { get }
     func login(email: String, password: String) async throws
     func signUp(profileImage: UIImage?, fullName: String, email: String, password: String) async throws
+    func changePassword(oldPassword: String, newPassword: String) async throws
     func signOut() throws
 }
 
@@ -35,19 +36,22 @@ final class AuthenticationManager: AuthenticationManagerProtocol {
         print("AuthenticationManager initialized with user ID: \(String(describing: userSession?.uid))")
     }
     
+    // MARK: - Login
     func login(email: String, password: String) async throws {
         do {
             let authResult = try await auth.signIn(withEmail: email, password: password)
-            print("Authentication Manager: User signed in succesfully")
+            logMessage("User logged in succesfully")
         } catch let error as NSError {
+            logMessage("failed to log in with error \(error.localizedDescription)")
             throw error.toAuthenticationError()
         }
     }
     
+    // MARK: - Sign Up
     func signUp(profileImage: UIImage?, fullName: String, email: String, password: String) async throws {
         do {
             let authResult = try await auth.createUser(withEmail: email, password: password)
-            print("Authentication Manager: User signed up with ID: \(authResult.user.uid)")
+            logMessage("User signed up with ID: \(authResult.user.uid)")
             
             try await userDataStoringManager.storeUserData(
                 userID: authResult.user.uid,
@@ -56,15 +60,51 @@ final class AuthenticationManager: AuthenticationManagerProtocol {
                 profileImage: profileImage
             )
             
-            print("Authentication Manager: User data saved successfully")
+            logMessage("User data saved successfully")
         } catch let error as NSError {
+            logMessage("failed to sign up with error \(error.localizedDescription)")
             throw error.toAuthenticationError()
         }
     }
     
+    // MARK: - Sign Out
     func signOut() throws {
         try auth.signOut()
-        print("Authentication Manager: current userSession: \(String(describing: userSession))")
-        print("Authentication Manager: User signed out successfully")
+        logMessage("User signed out successfully, current userSession: \(String(describing: userSession))")
+    }
+    
+    // MARK: - Change Password
+    func changePassword(oldPassword: String, newPassword: String) async throws {
+        guard let email = userSession?.email,
+        let user = auth.currentUser else {
+            logMessage("no current user signed in")
+            throw AuthenticationError.userNotFound
+        }
+        
+        let credential = EmailAuthProvider.credential(withEmail: email, password: oldPassword)
+        
+        try await reAuthenticate(user: user, with: credential)
+        
+        do {
+            try await user.updatePassword(to: newPassword)
+            logMessage("Updated password successfully")
+        } catch {
+            logMessage("failed to update password with error \(error.localizedDescription)")
+            throw AuthenticationError.passwordUpdateFailed
+        }
+    }
+    
+    // MARK: - Private Method
+    private func reAuthenticate(user: FirebaseAuth.User, with credential: AuthCredential) async throws {
+        do {
+            try await user.reauthenticate(with: credential)
+        } catch let error as NSError {
+            logMessage("failed to reauthenticate user with error \(error.localizedDescription)")
+            throw error.toAuthenticationError()
+        }
+    }
+    
+    private func logMessage(_ message: String) {
+        print("Authentication Manager: \(message)")
     }
 }
